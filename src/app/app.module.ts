965bdea0e20771af259c848f481359af20bb179c
Jorge Leite <jorgefernandodamasio@gmail.com>
2018-08-08 20:57:34 -0300

    First Commit, Nav Bar and initial style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DescriptionComponent } from './description/description.component';
+import { MediaComponent } from './media/media.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DescriptionComponent,
+    MediaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
